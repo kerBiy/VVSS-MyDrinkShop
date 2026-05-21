019fad87d74f6250bd13b7881797f81fa83b73ea
kerBiy <alexandrubalta222@gmail.com>
2026-05-21 18:31:15 +0300

    changed java version

--- src/main/java/drinkshop/receipt/ReceiptGenerator.java
+++ src/main/java/drinkshop/receipt/ReceiptGenerator.java
@@ -5,13 +5,14 @@ import drinkshop.domain.OrderItem;
 import drinkshop.domain.Product;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReceiptGenerator {
     public static String generate(Order o, List<Product> products) {
         StringBuilder sb = new StringBuilder();
         sb.append("===== BON FISCAL =====\n").append("Comanda #").append(o.getId()).append("\n");
         for (OrderItem i : o.getItems()) {
-            Product p = products.stream().filter((p1)->i.getProduct().getId()==p1.getId()).toList().get(0);
+            Product p = products.stream().filter((p1)->i.getProduct().getId()==p1.getId()).collect(Collectors.toList()).get(0);
             sb.append(p.getNume()+": ").append(p.getPret()).append(" x ").append(i.getQuantity()).append(" = ").append(i.getTotal()).append(" RON\n");
         }
         sb.append("---------------------\nTOTAL: ").append(o.getTotalPrice()).append(" RON\n=====================\n");
